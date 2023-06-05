4296e280238452dc7512192e5a6e56f813e79912
Shahriar Ahmed Tonmoy <119741677+Shahriar-Ahmed-Tonmoy@users.noreply.github.com>
2023-06-05 23:03:25 +0600

    Add files via upload

---
+++ Adjacency_Matrix.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int no_vertices;
+void printGraph(int adj[no_vertices][no_vertices])
+{
+	for(int i=0;i<no_vertices;i++)
+	{
+		for(int j =0;j<no_vertices;j++)
+		{
+			printf(" %d ",adj[i][j]);
+		}
+		printf("\n");
+	}
+
+}
+int main()
+{
+    int s,d;
+    printf("\nEnter the number of vertices: ");
+    scanf("%d", &no_vertices);
+    int adj[no_vertices][no_vertices];
+    for(int i=0; i<no_vertices; i++)
+        for(int j=0; j<no_vertices; j++)
+            adj[i][j]=0;
+    while(s!=-1 && d!=-1)
+    {
+        printf("Enter an edge from node (0 to %d) to node (0 to %d): ",no_vertices,no_vertices);
+        scanf("%d %d",&s, &d);
+        adj[s][d]=1;
+        adj[d][s]=1;
+    }
+    printf("\nNow,The Adjacency Matrix is:\n");
+    printGraph(adj);
+}
