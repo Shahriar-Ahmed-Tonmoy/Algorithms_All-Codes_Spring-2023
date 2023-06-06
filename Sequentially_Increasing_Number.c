2037fb3cb5efff59a5b6c503014ce925c3c59da6
Shahriar Ahmed Tonmoy <119741677+Shahriar-Ahmed-Tonmoy@users.noreply.github.com>
2023-06-06 06:16:37 +0600

    Add files via upload

---
+++ Sequentially_Increasing_Number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i, sum = 0;
+    for(i=10; i<=15; i++)
+    {
+        sum = sum+1;
+        printf("%d for sum=%d\n", i, sum);
+    }
+    printf("%d for sum = %d", i, sum);
+}
